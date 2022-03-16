ce7037497c65da641b1f82211957b9c15d72143a
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-03-16 16:56:20 -0300

    requesito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const findAnimal = species.find((element) => element.name === animal);
+  return findAnimal.residents.every((element) => element.age >= age);
 }
+console.log(getAnimalsOlderThan('lions'));
 
 module.exports = getAnimalsOlderThan;
