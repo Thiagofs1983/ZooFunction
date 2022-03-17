9c57f91e2cf2aa5dac850e9b1b5f7f1a6f53922d
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-03-17 14:13:38 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
+
+function getIdFirstSpecie(id) {
+  const firstEmployee = employees.find((element) => element.id === id);
+  return firstEmployee.responsibleFor[0];
+}
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const firstSpecie = species.find((element) => element.id === getIdFirstSpecie(id));
+  const { residents } = firstSpecie;
+  const oldestFromSpecie = residents.reduce((acc, curr) => {
+    if (acc.age < curr.age) return curr;
+    return acc;
+  });
+  return Object.values(oldestFromSpecie);
 }
+
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = getOldestFromFirstSpecies;
