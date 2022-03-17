dde16a32f3d812d3af0a125de776a21c203a88dc
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-03-17 09:58:08 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqu
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const count = {
+    child: entrants.filter((element) => element.age < 18).length,
+    adult: entrants.filter((element) => element.age >= 18 && element.age < 50).length,
+    senior: entrants.filter((element) => element.age >= 50).length,
+  };
+  return count;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const { child, adult, senior } = countEntrants(entrants);
+  const valueChild = child * prices.child;
+  const valueAdult = adult * prices.adult;
+  const valueSenior = senior * prices.senior;
+  const valueTotal = valueAdult + valueChild + valueSenior;
+  return valueTotal;
 }
+
+console.log(calculateEntry());
 
 module.exports = { calculateEntry, countEntrants };
