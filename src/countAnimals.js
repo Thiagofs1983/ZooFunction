94d60a811d95658b08ef7437fcc33f2e63b9f554
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-03-17 16:53:42 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,45 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+/*
+1- Se nenhum argumento for passado, retorna um objeto
+2- Objeto deve ter a chave com o nome de cada especie
+3- O valor do objeto deve ser o total de animais da espécie
+4- Recebendo como parametro um obj com chave specie, retorna a quantidade de animais daquela especie
+5- Se parametro Obj specie e sex, retorna a quantidade de animais daquela espécie e sexo
+*/
+const creatObjAnimals = () => {
+  const objAnimals = species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
+  return objAnimals;
+};
+
+const countPerSpecie = (animal) => {
+  const amountPerSpecie = species.find((element) => element.name === animal.specie)
+    .residents.length;
+  return amountPerSpecie;
+};
+console.log(countPerSpecie({ specie: 'penguins' }));
+
+const countPerSpecieAndSex = (animal) => {
+  const amountPerSpecieAndSex = species.find((element) => element.name === animal.specie)
+    .residents.filter((element) => element.sex === animal.sex).length;
+  return amountPerSpecieAndSex;
+};
+console.log(countPerSpecieAndSex({ specie: 'giraffes', sex: 'female' }));
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return creatObjAnimals();
+  }
+  if (Object.keys(animal).length === 1) {
+    return countPerSpecie(animal);
+  }
+  if (Object.keys(animal).length === 2) {
+    return countPerSpecieAndSex(animal);
+  }
 }
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
